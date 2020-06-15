2aaa589cb7e7a84710f2c27c24625db2099ae7df
carlhauck <carlhauck@gmail.com>
2020-06-15 18:57:41 -0500

    adds Genre and MovieGenre models and has_many through association methods, along with a genre_names Movie model method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,16 @@
 class Movie < ApplicationRecord
+  
+  has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
   validates :director, presence: true
   validates :title, presence: true
   validates :year, numericality: { greater_than: 1887 }
   validates :plot, presence: true
 
-  has_many :actors
+  def genre_names
+    genres.map { |genre| genre.name }
+  end
+
 end
